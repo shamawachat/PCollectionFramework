276ac65f3305ca7239492e1ee895b45f7377450f
shama <shamawachat@rediffmail.com>
2023-03-17 17:18:58 +0530

    Vector class completed!

--- PCollectionFramework/src/cf/list/VectorEx.java
+++ PCollectionFramework/src/cf/list/VectorEx.java
@@ -24,6 +24,41 @@ public class VectorEx
 		System.out.println("Capacity:\n"+v.capacity());
 		System.out.println("Size:\n"+v.size());
 		System.out.println("======================================");
+		
+		int n=(int)v.get(2);
+		System.out.println("Index 2 squared: "+n*n);
+		
+		Test t=(Test)v.get(6);
+		t.show();
+		
+//		System.out.println(v.contains("Sham Awachat"));
+		boolean res=v.contains("Kapil");
+		System.out.println("True/False: "+res);
+		
+		v.remove("Sham Awachat");
+		System.out.println("Vector Elements:\n"+v);
+		
+		v.set(7, "Sham");
+		System.out.println("Vector Elements:\n"+v);
+		
+		v.add(7, "Kapil");
+		System.out.println("Vector Elements:\n"+v);
+		
+		System.out.println("Printing Vector Elements using for-loop:");
+		for(int i=0;i<v.size();i++)
+		{
+			System.out.print(v.get(i)+" | ");
+		}
+		System.out.println();
+		System.out.println("---------------------------------------");
+		
+		System.out.println("Printing Vector Elements using for-each-loop:");
+		for(Object ob:v)
+		{
+			System.out.print(ob+" | ");
+		}
+		System.out.println();
+		System.out.println("---------------------------------------");
 	}
 	
 }
