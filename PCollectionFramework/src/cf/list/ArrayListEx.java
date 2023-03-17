d8f08e12a8e45d637ce4ff2507b239b50bcc51b5
shama <shamawachat@rediffmail.com>
2023-03-17 18:16:14 +0530

    Using main

--- PCollectionFramework/src/cf/list/ArrayListEx.java
+++ PCollectionFramework/src/cf/list/ArrayListEx.java
@@ -1,9 +1,29 @@
 package cf.list;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class ArrayListEx 
 {
 	public static void main(String[] args) 
 	{
+		ArrayList ar=new ArrayList<>();
 		
+		ar.add(1);
+		ar.add(1);
+		ar.add(2);
+		ar.add(null);
+		ar.add(22.7);
+		ar.add(null);
+		ar.add(new Test());
+		ar.add("Sham Awachat");
+		ar.add("Kapil");
+		ar.add("Bit2");
+		ar.add("Yeotmal");
+		
+		System.out.println("Array Elements:\n"+ar);
+//		System.out.println("Capacity:\n"+ar.capacity());
+		System.out.println("Size:\n"+ar.size());
+		System.out.println("======================================");
 	}
 }
